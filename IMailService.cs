2b716575e8fc101aecf873fc1c457a559ba26e2c
Zainalds <68240955+Zainalds@users.noreply.github.com>
2020-11-17 12:11:26 +0200

    Add files via upload

---
+++ IMailService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Authorization.Services
+{
+    public interface IMailService
+    {
+
+        Task SendEmailAsync(string toEmail, string subject, string content);
+    }
+
+    public class SendGridMailService : IMailService
+    {
+        private IConfiguration _configuration;
+
+        public SendGridMailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task SendEmailAsync(string toEmail, string subject, string content)
+        {
+            var apiKey = _configuration["SendGridKey"];
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("zainolnabi88@gmail.com", "Triton Express");
+            var to = new EmailAddress(toEmail);
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, content, content);
+            var response = await client.SendEmailAsync(msg);
+        }
+    }
+}
